3246c13970013827f3d7de11d8c6f4546f59b318
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-13 15:04:13 -0400

    test

--- woo-random-featured.php
+++ woo-random-featured.php
@@ -41,14 +41,13 @@ global $wpdb;
 
 $ids = $wpdb->get_col( "SELECT object_id FROM $wpdb->term_relationships WHERE term_taxonomy_id = 8" );
 
-/* if ( count( $ids ) > 1 ) 
+if ( count( $ids ) > 1 ) 
     return $ids;
 else
-    return $ids[0]; */
+    return $ids[0];
 
 
 
-var_dump($ids);
 
 
 }
