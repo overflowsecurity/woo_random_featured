ac612413c6dcfd93e29dc82c285e07367385104d
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-13 15:11:07 -0400

    test

--- woo-random-featured.php
+++ woo-random-featured.php
@@ -41,7 +41,7 @@ global $wpdb;
 
 $ids = $wpdb->get_col( "SELECT object_id FROM $wpdb->term_relationships WHERE term_taxonomy_id = 8" );
 
-foreach($ids as $id->object_id){
+foreach($ids as $id){
         return $id;
 }
 
