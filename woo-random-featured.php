9b7a69ae3a75e6f9851050c374b0d500d03df856
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-14 18:38:58 -0400

    test

--- woo-random-featured.php
+++ woo-random-featured.php
@@ -29,7 +29,11 @@ function plugin_settings_page_content(){
     
     ?><center><h1>WooCommerce Random Featured Products</h1></center><?php
 
-    echo "<form method='post' action=''>" . settings_fields( 'jt_wrf_admin_settings' ) . do_settings_sections( 'woo_random_featured' ) . "</form>";
+    ?>
+    <form method='post' action=''>
+    <?php settings_fields( 'jt_wrf_admin_settings' );
+    do_settings_sections( 'woo_random_featured' );
+    ?></form><?php
     //RunFeatured();
     //echo "<h2>Done!</h2>";
 
