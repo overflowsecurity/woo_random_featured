ae59430c0977c123a9078e6e5422af68d1145a3c
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-14 22:25:59 -0400

    test

--- woo-random-featured.php
+++ woo-random-featured.php
@@ -39,7 +39,8 @@ function plugin_settings_page_content(){
     //RunFeatured();
     //echo "<h2>Done!</h2>";
 
-    echo get_option( 'jt_when_to_change' );
+    $test = get_option( 'jt_when_to_change' );
+    var_dump($test);
 }
 
 function jt_wrf_display_options(){
